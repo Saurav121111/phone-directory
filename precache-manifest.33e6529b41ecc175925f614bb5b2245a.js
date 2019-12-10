a3873c514d16c74dd43d92a36c0bd784c2633e64
Saurav12111 <sauravdhiman70@gmail.com>
2019-12-10 15:08:22 +0530

    Updates

---
+++ precache-manifest.33e6529b41ecc175925f614bb5b2245a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35b912b765ed207c54e8",
+    "url": "/phone-directory/static/js/runtime~main.e30a5ef0.js"
+  },
+  {
+    "revision": "b5781511c8d1796dcfce",
+    "url": "/phone-directory/static/js/main.9abd3adc.chunk.js"
+  },
+  {
+    "revision": "ff82c14c6c9f7ba1f615",
+    "url": "/phone-directory/static/js/2.9ee0d791.chunk.js"
+  },
+  {
+    "revision": "b5781511c8d1796dcfce",
+    "url": "/phone-directory/static/css/main.1acb31aa.chunk.css"
+  },
+  {
+    "revision": "71d0a0d1127ed9dc9a9889feb12d51bd",
+    "url": "/phone-directory/index.html"
+  }
+];
